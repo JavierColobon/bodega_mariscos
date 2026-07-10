91b4a1083c6fc00b4c8c64953b12367d2fa36ebc
Javier Colobon <javiercolobon75@gmail.com>
2026-07-10 01:47:09 -0500

    Registro de admin en embarcaciones

--- embarcaciones/admin.py
+++ embarcaciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Embarcacion
 
-# Register your models here.
+@admin.register(Embarcacion)
+class EmbarcacionAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'matricula', 'capacidad_kg', 'tipo', 'activo', 'fecha_registro']
+    list_filter = ['activo', 'tipo']
+    search_fields = ['nombre', 'matricula']
+    ordering = ['nombre']
