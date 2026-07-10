7de92411967d700daa21f4ae011bca297b4553a1
Javier Colobon <javiercolobon75@gmail.com>
2026-07-10 01:58:10 -0500

    Registro de los admin de configuracion y recepcion

--- configuracion/admin.py
+++ configuracion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import PrecioEspecie
 
-# Register your models here.
+@admin.register(PrecioEspecie)
+class PrecioEspecieAdmin(admin.ModelAdmin):
+    list_display = ['especie', 'precio_por_kg', 'vigente_desde', 'vigente_hasta', 'activo']
+    list_filter = ['activo', 'vigente_desde']
+    search_fields = ['especie']
+    ordering = ['especie']
--- recepciones/admin.py
+++ recepciones/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import RecepcionItem
 
-# Register your models here.
+@admin.register(RecepcionItem)
+class RecepcionItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'faena', 'especie', 'peso_kg', 'cantidad', 
+        'unidad_medida', 'precio_unitario', 'subtotal', 'fecha_registro'
+    ]
+    list_filter = ['especie', 'unidad_medida', 'fecha_registro']
+    search_fields = ['faena__codigo_faena', 'especie']
+    readonly_fields = ['subtotal', 'fecha_registro']
