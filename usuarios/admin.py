0de54688d385de9d67080c6f40c9085cd398c3f1
Javier Colobon <javiercolobon75@gmail.com>
2026-07-10 01:44:31 -0500

    Registro de admin en usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'rol', 'activo']
+    list_filter = ['rol', 'activo', 'is_staff']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información Adicional', {'fields': ('rol', 'telefono', 'activo')}),
+    )
